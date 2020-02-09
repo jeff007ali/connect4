8454b19fd41e0f94240fad91ea5abaf144e4ba03
jeff007ali <jagharia@gmail.com>
2020-02-09 10:19:03 +0530

    Added pygame code to draw board and get column value using mouse click position

--- connect4_terminal.py
+++ connect4_terminal.py
@@ -1,12 +1,7 @@
 import numpy as np
-import pygame
-import sys
-import math
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-BLUE = (0, 0, 255)
-BLACK = (0, 0, 0)
 
 def create_board():
     board = np.zeros((ROW_COUNT, COLUMN_COUNT))
@@ -74,63 +69,33 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
                 return True
 
-def draw_board(board):
-    for r in range(ROW_COUNT):
-        for c in range(COLUMN_COUNT):
-            pygame.draw.rect(screen, BLUE, (c*SQUARE_SIZE, r*SQUARE_SIZE+SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-            pygame.draw.circle(screen, BLACK, (c*SQUARE_SIZE+SQUARE_SIZE//2, r*SQUARE_SIZE+SQUARE_SIZE+SQUARE_SIZE//2), RADIUS)
-
-
 
 board = create_board()
 print(board)
 running = True
 turn = 0
 
-pygame.init()
-
-SQUARE_SIZE = 100
-width = COLUMN_COUNT * SQUARE_SIZE
-height = (ROW_COUNT + 1) * SQUARE_SIZE
-size = (width, height)
-
-RADIUS = int(SQUARE_SIZE/2 - 5)
-
-screen = pygame.display.set_mode(size)
-draw_board(board)
-pygame.display.update()
-
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-            # We can make running variable as False to quit the game
-            # running = False
+    # Ask for Player 1 input
+    if turn % 2 == 0:
+        selection = int(input("Player 1 make your selection (1-7): "))
+        piece = 1
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pass
+    # Ask for Player 2 input
+    else:
+        selection = int(input("Player 2 make your selection (1-7): "))
+        piece = 2
 
-            posx = event.pos[0]
-            col = posx // SQUARE_SIZE
-            print(col)
+    # Reduce column by 1 because column index start from "0" and Player's selection start from "1" 
+    col = selection -1
 
-            # Ask for Player 1 input
-            if turn % 2 == 0:
-                # posx = event.pos[0]
-                # col = posx // SQUARE_SIZE
-                piece = 1
+    if is_valid_location(board, col):
+        row = get_next_open_row(board, col)
+        drop_piece(board, row, col, piece)
 
-            # Ask for Player 2 input
-            else:
-                piece = 2
+        if winning_move(board, piece):
+            print("Player {} wins!!!".format(piece))
+            running = False
 
-            if is_valid_location(board, col):
-                row = get_next_open_row(board, col)
-                drop_piece(board, row, col, piece)
-
-                if winning_move(board, piece):
-                    print("Player {} wins!!!".format(piece))
-                    running = False
-
-            turn += 1
-            print(board)
+    turn += 1
+    print(board)
